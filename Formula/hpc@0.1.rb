3bdf85aaf7dedc617d786eb328b5ccc8ea44fa40
Alex <sizykh.a.d@gmail.com>
2026-01-03 10:40:31 -0600

    0.1 fix

--- Formula/hpc@0.1.rb
+++ Formula/hpc@0.1.rb
@@ -6,14 +6,6 @@ class HpcAT01 < Formula
   license "AGPL-3.0-only"
   keg_only :versioned_formula
 
-  bottle do
-    root_url "https://github.com/hpcd-dev/homebrew-hpc/releases/download/hpc-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f2f3b9271b38130ac98535e2276b8fd43435d7f51c7eb699c4acf201c7a0d91e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a6c7b50f376533e97ffe597a107c50eba9b4293cce3f135bfaddce9d4d092d68"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f629f94d0cbd0984813c3fa9890330e0ede4fff98917a579ee4406e18d0f0d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8ccfa9238ea6523061a1e229f07f5156b692d5b10b6a4969356f7f60fd4d34a"
-  end
-
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "protobuf" => :build
